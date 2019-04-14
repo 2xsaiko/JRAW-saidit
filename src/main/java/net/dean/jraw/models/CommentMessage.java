5172a8bf261020e32488a4fb0cc129bce22a85da
therealfarfetchd <git@therealfarfetchd.dev>
2019-04-14 22:59:47 +0200

    saidit-style voting

--- src/main/java/net/dean/jraw/models/CommentMessage.java
+++ src/main/java/net/dean/jraw/models/CommentMessage.java
@@ -1,8 +1,8 @@
 package net.dean.jraw.models;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import net.dean.jraw.models.meta.JsonProperty;
 import net.dean.jraw.models.meta.Model;
-import com.fasterxml.jackson.databind.JsonNode;
 
 /**
  * This class represents a message that appears in a user's inbox because another user has replied to his or her comment.
@@ -24,12 +24,12 @@ public final class CommentMessage extends Message {
 
     /** Gets the way the logged in user voted on the comment that this message represents. */
     @JsonProperty(nullable = true)
-    public VoteDirection getVote() {
+    public VoteState getVote() {
         JsonNode likes = getDataNode().get("likes");
-        if (likes.isNull()) {
-            return VoteDirection.NO_VOTE;
-        }
+        boolean likesState = !likes.isNull() && likes.booleanValue();
+        JsonNode dislikes = getDataNode().get("dislikes");
+        boolean dislikesState = !dislikes.isNull() && dislikes.booleanValue();
 
-        return likes.booleanValue() ? VoteDirection.UPVOTE : VoteDirection.DOWNVOTE;
+        return VoteState.of(likesState, dislikesState);
     }
 }
